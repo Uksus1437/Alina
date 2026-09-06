ff1b386e7401622a27d9f8ef1c44b8346b3daf99
Uksus1437 <116167254+Uksus1437@users.noreply.github.com>
2026-09-06 12:20:51 +0300

    21

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-'''Алгоритм вычисления значения функции F(n), где n — натуральное число, задан следующими соотношениями:
+'''
+Алгоритм вычисления значения функции F(n), где n - целое число, задан следующими соотношениями:
 
-F(n) = n + 1 при n ≤ 2;
-F(n) = 2*F(n - 1) + F(n - 2) при n > 2.
+F(n) = n при n < 10
+F(n) = 3n + F(n - 3), если n >= 10
 
-Чему равно значение функции F(4)? В ответе запишите только натуральное число.'''
+Чему равно значение выражения (F(6250) + 2 * F(6244)) / F(6238)?
 
-def F(n):
-  if n <= 2:
-    return n+1
+В ответе запишите целую часть полученного числа.
+'''
+
+from sys import *
+
+setrecursionlimit(10**6)
+
+
+def F (n):
+  if n < 10:
+    return n
   else:
-    return 2*F(n - 1) + F(n - 2)
-print (F(4))
+    return 3*n + F (n -3)
+print ((F(6250)+2*F(6244))/F(6238))
+
+
+
+#     return 3*n + F (n -3)
+#   [Previous line repeated 995 more times]
+#   File "/Users/mvgordeev/Documents/GitHub/Alina/main.py", line 14, in F
+#     if n < 10:
+# RecursionError: maximum recursion depth exceeded in comparison
